3fc3ade332a04147d7cc2cc21efb325e44300bf3
Patrick Dawson <patrick.dawson79@gmail.com>
2023-10-14 11:10:57 +0200

    chore(pg): Initial commit

---
+++ 01-observables.js
@@ -0,0 +1,13 @@
+import { Observable } from 'rxjs';
+
+const observable = new Observable(subscriber => {
+  subscriber.next(1);
+  subscriber.next(2);
+  subscriber.next(3);
+
+  return () => {
+    console.log('unsubscribe');
+  }
+});
+
+observable.subscribe(value => console.log(value));
